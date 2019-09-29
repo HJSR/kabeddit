e866d2e2ee6fbd036548d05d980d716620bf91e7
hjsr <hendyjosue@akimad.com>
2019-09-29 14:14:34 +0200

    Added top padding on desktop

--- src/modules/GalleryView.tsx
+++ src/modules/GalleryView.tsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Masonry from 'react-masonry-component';
 import InfiniteScroll from 'react-infinite-scroller';
+import styled from 'styled-components';
+import { Spin, Row, BackTop } from 'antd';
 
 import getPosts from '../services/getPosts';
 import Post from '../components/Post';
-import { Spin, Row, BackTop } from 'antd';
+
+const PostsContainer = styled.div`
+	@media (min-width: 769) {	
+		padding-top: 50px;
+	}
+`;
 
 const GalleryView = (props) => {
 	const { subreddits, order, time, showNSFW, showThumbnails } = props;
@@ -31,35 +38,37 @@ const GalleryView = (props) => {
 	}, [order, subreddits, time]);
 
 	return (
-		<InfiniteScroll
-			pageStart={0}
-			loadMore={getMorePosts}
-			hasMore={true || false}
-			threshold={5000}
-			loader={
-				<Row
-					type="flex"
-					justify="center"
-					align="middle"
-					style={{ height: 200 }}
-				>
-					<Spin size="large" />
-				</Row>
-			}
-		>
-			<Masonry>
-				{posts ? posts.map((post) => {
-					if (!showNSFW && post.over_18 === true) return null;
-					return (
-						<Post	
-							key={post.permalink}
-							post={post}
-							showThumbs={showThumbnails}
-						/>)
-				}): null}
-			</Masonry>
-			<BackTop />
-		</InfiniteScroll>
+		<PostsContainer>
+			<InfiniteScroll
+				pageStart={0}
+				loadMore={getMorePosts}
+				hasMore={true || false}
+				threshold={5000}
+				loader={
+					<Row
+						type="flex"
+						justify="center"
+						align="middle"
+						style={{ height: 200 }}
+					>
+						<Spin size="large" />
+					</Row>
+				}
+			>
+				<Masonry>
+					{posts ? posts.map((post) => {
+						if (!showNSFW && post.over_18 === true) return null;
+						return (
+							<Post	
+								key={post.permalink}
+								post={post}
+								showThumbs={showThumbnails}
+							/>)
+					}): null}
+				</Masonry>
+				<BackTop />
+			</InfiniteScroll>
+		</PostsContainer>
 	)
 }
 export default GalleryView;
