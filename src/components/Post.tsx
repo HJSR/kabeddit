efd1f04b569488d813ad56f7318f5bd7f172a0a2
hjsr <hendyjosue@akimad.com>
2019-09-29 02:57:32 +0200

    Adjusted responsive card appaeranceadsa

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,19 +7,22 @@ const { Meta } = Card;
 const PostCard = styled(Card)`
 	img { max-width: 100% }
 	width: 50%;
-	@media (min-width: 769px) {
+	@media (min-width: 1280px) {
 		width: 25%;
 	}
 
 	.ant-card-body {
-		position: absolute;
-		bottom: 0;
+		position: static;
 		background: white;
-		left: 0;
-		right: 0;
-		opacity: 0;
 		transition: all 0.15s;
-		box-shadow: 0 -2px 8px rgba(0, 0, 0, 0.09);
+		@media (min-width: 769px) {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			opacity: 0;
+			box-shadow: 0 -2px 8px rgba(0, 0, 0, 0.09);
+		}
 	}
 	&:hover {
 		.ant-card-body {
