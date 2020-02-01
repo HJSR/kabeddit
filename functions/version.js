98aeee24a787ed58dd619c17cfa0440451ae8c4d
hjsr <hendyjosue@akimad.com>
2020-02-01 20:05:37 +0100

    using callback

--- functions/version.js
+++ functions/version.js
@@ -1,8 +1,8 @@
-exports.handler = async (event, context) => {
-	return {
+exports.handler = async (event, context, callback) => {
+	return callback (null, {
 		statusCode: 200,
 		body: {
 			version: '1.0.0'
 		}
-	}
+	})
 }
