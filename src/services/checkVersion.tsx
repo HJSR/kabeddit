3ce8d05a1e67efa1ceeb0aa4394c546d8601535a
hjsr <hendyjosue@akimad.com>
2020-02-01 20:34:01 +0100

    version bump check

--- src/services/checkVersion.tsx
+++ src/services/checkVersion.tsx
@@ -1,9 +1,27 @@
+import React from 'react';
 import axios from 'axios';
 import pjson from '../../package.json';
+import { notification, Button } from 'antd';
 const { version } = pjson;
 
 const checkVersion = async () => {
-	const serverVersion = await axios.get('/.netlify/functions/version');
-	console.log(serverVersion, version);
+	const res = await axios.get('/.netlify/functions/version');
+	if (!res || !res.data || res.status !== 200) return null;
+	const serverVersion = res.data;
+	if (version !== serverVersion) {
+		notification.info({
+			message: 'New version available',
+			description: (
+				<Button 
+					type="link"
+					onClick={() => { window.location.reload() }}
+					style={{ paddingLeft: 0 }}
+				>
+					Click here to restart
+				</Button>
+			),
+			duration: null,
+		})
+	}
 }
 export default checkVersion;
