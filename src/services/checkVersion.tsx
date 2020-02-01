57e99abbbc0ec87b8f96735d02faa7f53019a139
hjsr <hendyjosue@akimad.com>
2020-02-01 20:34:40 +0100

    comments

--- src/services/checkVersion.tsx
+++ src/services/checkVersion.tsx
@@ -5,9 +5,11 @@ import { notification, Button } from 'antd';
 const { version } = pjson;
 
 const checkVersion = async () => {
+	// Checks version on the function (hardcoded)
 	const res = await axios.get('/.netlify/functions/version');
 	if (!res || !res.data || res.status !== 200) return null;
 	const serverVersion = res.data;
+	// compares it with the package.json version
 	if (version !== serverVersion) {
 		notification.info({
 			message: 'New version available',
