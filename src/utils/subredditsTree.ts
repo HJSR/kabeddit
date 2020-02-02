70dc52a1fce667bff994bf4eca3b3b9e703cd6e1
hjsr <hendyjosue@akimad.com>
2020-02-02 16:54:29 +0100

    default subreddits

--- src/utils/subredditsTree.ts
+++ src/utils/subredditsTree.ts
@@ -7,6 +7,30 @@ const subreddit = (name: string) => ({
 
 const subredditsTree = [
 	{
+		...subreddit('General'),
+		children: [
+			subreddit('CityPorn'),
+			subreddit('VillagePorn'),
+			subreddit('SpacePorn'),
+			subreddit('wallpapers'),
+			subreddit('MobileWallpaper'),
+			subreddit('Verticalwallpapers'),
+			subreddit('wallpaper'),
+		],
+	},
+	{
+		...subreddit('Nature'),
+		children: [
+			subreddit('EarthPorn'),
+			subreddit('BeachPorn'),
+			subreddit('WaterPorn'),
+			subreddit('SkyPorn'),
+			subreddit('WeatherPorn'),
+			subreddit('BotanicalPorn'),
+			subreddit('LakePorn'),
+		],
+	},
+	{
 		...subreddit('Anime'),
 		children: [
 			subreddit('AnimePhoneWallpapers'),
@@ -21,21 +45,24 @@ const subredditsTree = [
 			subreddit('streetmoe'),
 		],
 	},
-	{
-		...subreddit('Nature'),
-		children: [
-			subreddit('EarthPorn'),
-		],
-	},
 ];
 const defaultSubreddits = [
-	'moescape',
-	'streetmoe',
-	'awwnime',
-	'twodeeart',
-	'AnimeWallpaper',
-	'AnimePhoneWallpapers',
-	'Pixiv'
+	"MobileWallpaper",
+	"nocontext_wallpapers",
+	"wallpapers",
+	"cityporn",
+	"villageporn",
+	"spaceporn",
+	"mobilewallpaper",
+	"verticalwallpapers",
+	"wallpaper",
+	"earthporn",
+	"beachporn",
+	"waterporn",
+	"skyporn",
+	"weatherporn",
+	"botanicalporn",
+	"lakeporn",
 ]
 export { defaultSubreddits };
 export default subredditsTree;
